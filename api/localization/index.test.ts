c92fff724f5a8c336090780e93c604f0d08217e4
logan-anderson <landerson2@upei.ca>
2020-07-23 12:39:18 -0300

    added tests

--- api/localization/index.test.ts
+++ api/localization/index.test.ts
@@ -1,59 +1,69 @@
 import { Locale, LocalizationApi } from "./index"
 
 describe("Localization API", () => {
-  const LocalAPI = new LocalizationApi()
-  it("Can be instantiated", () => {
-    expect(LocalAPI).not.toBeNull()
+  const localAPI = new LocalizationApi()
+  it("has a default locale", () => {
+    expect(localAPI.getLocale()).not.toBeNull()
   })
-  it("stores a default locale", () => {
-    expect(LocalAPI).not.toBeNull()
+  describe("#setLocal(newLocale)", () => {
+    it("updates the locale to be newLocale", () => {
+      const newLocal: Locale = {
+        language: "en",
+        encoding: "utf-8",
+      }
+      localAPI.setLocale(newLocal)
+      expect(localAPI.locale).toBe(newLocal)
+    })
   })
-  it("stores a locale", () => {
-    const setLocale: Locale = {
-      language: "en",
-      encoding: "utf-8",
-    }
-    LocalAPI.setLocale(setLocale)
-    expect(LocalAPI.locale).toBe(setLocale)
-  })
-  it("format's the locale when retrieved", () => {
-    const vals: { locale: Locale; expected: string }[] = [
-      {
-        locale: {
-          language: "en",
-          encoding: "utf-8",
+  describe("#getLocale()", () => {
+    it("format's the locale to mathch language[_region][.encoding][@modifier]when retrieved", () => {
+      const rand = Math.floor(Math.random() * 100)
+      const vals: { locale: Locale; expected: string }[] = [
+        {
+          locale: {
+            language: "en",
+            encoding: "utf-8",
+          },
+          expected: "en.utf-8",
         },
-        expected: "en.utf-8",
-      },
-      {
-        locale: {
-          language: "en",
-          encoding: "utf-8",
-          region: "CA",
+        {
+          locale: {
+            language: "en",
+            encoding: `utf-${rand}`,
+          },
+          expected: `en.utf-${rand}`,
         },
-        expected: "en_CA.utf-8",
-      },
-      {
-        locale: {
-          language: "en",
-          encoding: "utf-8",
-          region: "CA",
+        {
+          locale: {
+            language: "en",
+            encoding: "utf-8",
+            region: "CA",
+          },
+          expected: "en_CA.utf-8",
         },
-        expected: "en_CA.utf-8",
-      },
-      {
-        locale: {
-          language: "en",
-          encoding: "utf-8",
-          region: "CA",
-          modifiers: ["test", "test2"],
+        {
+          locale: {
+            language: "en",
+            encoding: "utf-8",
+            region: "CA",
+            modifiers: ["test"],
+          },
+          expected: "en_CA.utf-8@test",
         },
-        expected: "en_CA.utf-8@test@test2",
-      },
-    ]
-    vals.forEach((val) => {
-      LocalAPI.setLocale(val.locale)
-      expect(LocalAPI.getLocale()).toEqual(val.expected)
+        {
+          locale: {
+            language: "en",
+            encoding: "utf-8",
+            region: "CA",
+            modifiers: ["test", "test2"],
+          },
+          expected: "en_CA.utf-8@test@test2",
+        },
+      ]
+      vals.forEach((val) => {
+        localAPI.setLocale(val.locale)
+        expect(localAPI.getLocale()).toEqual(val.expected)
+      })
     })
   })
 })
